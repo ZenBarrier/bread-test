1b1b93d8951801584665fba465d03070bf90c8ac
ZenBarrier <anthony.j.barrera@gmail.com>
2019-01-17 23:41:17 -0500

    button stuff

--- myproject/myproject.py
+++ myproject/myproject.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
+from settings import BREAD_SECRET
 
 """ try:
     import os
@@ -14,9 +15,27 @@ application = Flask(__name__, static_folder="static")
 def static_files(path):
     return send_from_directory('static',path)
 
-@application.route("/")
-def chat():
+@application.route("/hello")
+def hello():
     return "Hello World"
+
+@application.route("/tax", methods=['POST'])
+def getTax():
+    taxes = dict()
+    taxes["NY"] = .05
+    request_json = request.get_json()
+    state = request_json["shippingAddress"]["state"]
+    price = request_json["total"]
+    tax_amount = price * taxes.get(state, 0)
+    return jsonify(tax_amount)
+
+@application.route("/shipping", methods=['POST'])
+def getShipping():
+    shipping = [
+        {"typeId":"2day", "cost":2500, "type":"Two Day Shipping"},
+        {"typeId":"1day", "cost":7500, "type":"Overnight Shipping"}
+    ]
+    return jsonify(shipping)
 
 if __name__ == "__main__":
     application.run()
--- myproject/static/myscript.js
+++ myproject/static/myscript.js
@@ -1,6 +1,101 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
+var opts = {
+  buttonId: 'bread-checkout-btn',
+  actAsLabel: false,
+  asLowAs: true,
+  items: [
+    {
+      name: 'Couch',
+      price: 10000,
+      sku: 'COUCH123',
+      quantity: 1,
+      imageUrl: '[REPLACEMEWITHAREALURL]',
+      detailUrl: '[REPLACEMEWITHAREALURL]',
+    }]
+};
+bread.checkout(opts);
+
+opts.done = function (err, tx_token) {
+  if (err) {
+    console.error("There was an error: " + err);
+    return;
+  }
+  if (tx_token !== undefined) {
+    console.log(tx_token);
+    var i = document.createElement('input');
+    i.type = 'hidden';
+    i.name = 'token';
+    i.value = tx_token;
+    var f = document.createElement('form');
+    f.action = '[REPLACEMEWITHSERVICEURL]';
+    f.method = 'POST';
+    f.appendChild(i);
+    document.body.appendChild(f);
+    f.submit();
+  }
+  return;
+};
+bread.checkout(opts);
+
+opts.calculateTax = function (shippingContact, billingContact, callback) {
+  $.ajax({
+    url: '/tax',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      shippingAddress: shippingContact,
+      total: opts.items[0].price * opts.items[0].quantity
+    })
   })
+    .done(function (data) {
+      callback(null, data);
+    })
+    .fail(function (err) {
+      callback(err);
+    });
+};
+opts.calculateShipping = function (shippingContact, callback) {
+  $.ajax({
+    url: '/shipping',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      shippingAddress: shippingContact,
+      total: opts.items[0].price * opts.items[0].quantity
+    })
+  })
+    .done(function (data) {
+      callback(null, data);
+    })
+    .fail(function (err) {
+      callback(err);
+    });
+};
+
+opts.onCustomerClose = function (err, custData) {
+  if (err !== null) {
+    console.error("An error occurred getting customer close data.");
+    return;
+  }
+  var customerEmail = custData.email;
+  var qualState = custData.state;
+  switch (qualState) {
+    case 'PREQUALIFIED':
+      console.log(customerEmail + " was prequalified for financing.");
+      alert("Succesfully Financed!")
+      break;
+    case 'PARTIALLY_PREQUALIFIED':
+      console.log(customerEmail + " was partially prequalified for financing.");
+      break;
+    case 'NOT_PREQUALIFIED':
+      console.log(customerEmail + " was not prequalified for financing.");
+      alert("Sorry, Declined!")
+      break;
+    case 'ABANDONED':
+      if (customerEmail === undefined || customerEmail === null) {
+        console.log("Unknown customer abandoned their prequalification attempt.");
+      } else {
+        console.log(customerEmail + " abandoned their prequalification attempt.");
+      }
+      break;
+  }
+}
